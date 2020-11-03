11bf7ff12075e9b9be7cf72a65d422675c1d76de
Amit Lin <amitl@totango.com>
2020-11-03 12:34:06 +0200

    added ability to use query() instead of scan()
    should reduce costs significantly

--- interfaces/database/db.js
+++ interfaces/database/db.js
@@ -1,19 +1,18 @@
 const config = require("config");
-const Dynamodb = require('./dynamodb');
+const Dynamodb = require("./dynamodb");
 
 let _db;
 
 const initialize = () => {
-    if (config.has("dynamodb"))
-        _db = new Dynamodb(config.get("dynamodb.tableName"), config.get('dynamodb.region'));
+    if (config.has("dynamodb")) _db = new Dynamodb(config.get("dynamodb.tableName"), config.get("dynamodb.region"), config.dynamodb.scanStateIndex);
     // else if (some other db interface)
-}
+};
 
 const getDb = () => {
     return _db;
-}
+};
 
 module.exports = {
     initialize,
-    getDb
-}
+    getDb,
+};
